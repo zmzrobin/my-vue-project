0eac64df248c313bac1853f15bde77bf47242767
mingzhou zheng <mingzhouzheng@mingzhoudeMacBook-Pro.local>
2018-12-07 16:40:23 +0800

    no message

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-link to="/">首页</router-link>
+    <router-link to="/todo">todo</router-link>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style lang="less">
+  @import "css/common.less";
+
+  #app {
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import todo from "@/view/todo.vue"
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/todo',
+      name: 'todo',
+      component: todo
+    }
+  ]
+})
